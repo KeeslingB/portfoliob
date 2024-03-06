2ff7903e70827c8d2009bce28e68b6976b938952
unknown <Breke64@gmail.com>
2024-03-05 23:18:38 -0500

    footer background and positioning - still pondering with other styling issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
+
+
+
+
 export default function Footer(){
 
   return (
     <>
     <footer className="footer">
-      <p className="footer-text">Made by Brendan Keesling - Code available at <a href="https:/github.com/keeslingb"> GitHub</a></p>
+      <p className="footer-text">Made by Brendan Keesling - Code available on <a href="https:/github.com/keeslingb">GitHub</a></p>
     </footer>
     </>
   )
