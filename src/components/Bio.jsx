8c51f365354bdf42858516ddf7a182a6d063853d
unknown <Breke64@gmail.com>
2024-03-07 16:14:25 -0500

    added filler text inside bio section

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,7 +7,7 @@ export default function Bio(){
   return (
     <>
     <div>
-      <h2>Bio.</h2>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, maiores quisquam at, ut quo ipsa magnam doloribus aliquid tenetur, corporis veritatis ipsum itaque libero ad! Voluptatum cum culpa quas quis!</h2>
     </div>
     </>
   )
