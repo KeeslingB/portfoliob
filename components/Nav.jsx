325676976c952b7beab713bc602fc45dc8850462
unknown <Breke64@gmail.com>
2024-03-26 22:04:39 -0400

    issues with navlink still

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,38 @@
-
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
-
-  return(
-    <>
-    Nav
-    </>
-  )
+  return (
+    <ul>
+      <li>
+        <NavLink
+          to="/"
+          style={({ isActive }) => {
+            return isActive ? { color: "plum" } : {};
+          }}
+        >
+          Home
+        </NavLink>
+      </li>
+      {/* <li>
+        <NavLink
+          to="/projects"
+          style={({ isActive }) => {
+            return isActive ? { color: "plum" } : {};
+          }}
+        >
+          Projects
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact-me"
+          style={({ isActive }) => {
+            return isActive ? { color: "plum" } : {};
+          }}
+        >
+          Contact Me
+        </NavLink>
+      </li> */}
+    </ul>
+  );
 }
